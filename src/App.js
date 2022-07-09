758029756ad57f3b4ddf540567971d771d96210e
Aaron Ang <67321817+aaron-ang@users.noreply.github.com>
2022-07-09 15:57:27 +0800

    remove exports line

--- src/App.js
+++ src/App.js
@@ -34,7 +34,6 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-Object.defineProperty(exports, "__esModule", { value: true });
 const react_1 = __importStar(require("react"));
 const LetterSquare_1 = __importDefault(require("./driver/LetterSquare"));
 const Stack_1 = __importDefault(require("@mui/material/Stack"));
