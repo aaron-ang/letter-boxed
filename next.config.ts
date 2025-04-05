2bff147c1f63f352c1da81ff706c53e3fc585c97
Aaron Ang <aaron.angyd@gmail.com>
2025-04-05 11:32:00 -0700

    chore: Next.js config for static site generation

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export", // Outputs a Single-Page Application (SPA)
   distDir: "build", // Changes the build output directory to `build`
-  crossOrigin: "anonymous",
+  basePath: "/letter-boxed",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
